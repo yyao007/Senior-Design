a11ca5f48537384fe822d0fcc3c209ec8fb021d2
apple <apple@ucrwpa2-1-170-62.bulk.ucr.edu>
2015-05-08 15:48:31 -0700

    deleted all the files

--- chasim/snprenum_1207a.m
+++
@@ -1,9 +0,0 @@
-function [ snpnew, err, errmsg ] = snprenum_1207a( snp, renum  )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
- snpnew = [];
- err = 0;
- errmsg = '';
-
-end
-
